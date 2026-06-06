4991d556560f32080674286a2314fef20bf7018b
Prateek Prakash <tools.prateek@gmail.com>
2026-06-06 20:38:52 +0530

    feat: rename website folder to download and configure rewrite for /download clean URL

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,14 @@ const nextConfig = {
       },
     ];
   },
+  async rewrites() {
+    return [
+      {
+        source: "/download",
+        destination: "/download/index.html",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
